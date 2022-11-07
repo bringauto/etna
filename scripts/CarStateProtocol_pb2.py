d221e07c510083a95855b802239d46482b4f2c6b
Jiri Zak <jiri.zak@bringauto.com>
2022-11-07 12:09:41 +0100

    Implement protocol 1.2

--- scripts/CarStateProtocol_pb2.py
+++ scripts/CarStateProtocol_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x16\x43\x61rStateProtocol.proto\x12\x10\x43\x61rStateProtocol\"\x86\x03\n\tCarStatus\x12\x38\n\ttelemetry\x18\x01 \x01(\x0b\x32%.CarStateProtocol.CarStatus.Telemetry\x12\x30\n\x05state\x18\x02 \x01(\x0e\x32!.CarStateProtocol.CarStatus.State\x12$\n\x04stop\x18\x03 \x01(\x0b\x32\x16.CarStateProtocol.Stop\x1a`\n\tTelemetry\x12\r\n\x05speed\x18\x01 \x01(\x01\x12\x0c\n\x04\x66uel\x18\x02 \x01(\x01\x12\x36\n\x08position\x18\x03 \x01(\x0b\x32$.CarStateProtocol.CarStatus.Position\x1a\x41\n\x08Position\x12\x10\n\x08latitude\x18\x01 \x01(\x01\x12\x11\n\tlongitude\x18\x02 \x01(\x01\x12\x10\n\x08\x61ltitude\x18\x03 \x01(\x01\"B\n\x05State\x12\x08\n\x04IDLE\x10\x00\x12\t\n\x05\x44RIVE\x10\x01\x12\x0b\n\x07IN_STOP\x10\x02\x12\x0c\n\x08OBSTACLE\x10\x03\x12\t\n\x05\x45RROR\x10\x04\"\x96\x01\n\nCarCommand\x12%\n\x05stops\x18\x01 \x03(\x0b\x32\x16.CarStateProtocol.Stop\x12\x33\n\x06\x61\x63tion\x18\x02 \x01(\x0e\x32#.CarStateProtocol.CarCommand.Action\",\n\x06\x41\x63tion\x12\r\n\tNO_ACTION\x10\x00\x12\x08\n\x04STOP\x10\x01\x12\t\n\x05START\x10\x02\"\x12\n\x04Stop\x12\n\n\x02to\x18\x01 \x01(\tB>Z!../internal/pkg/ba_proto;ba_proto\xaa\x02\x18Google.Protobuf.ba_protob\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x16\x43\x61rStateProtocol.proto\x12\x10\x43\x61rStateProtocol\"\x86\x03\n\tCarStatus\x12\x38\n\ttelemetry\x18\x01 \x01(\x0b\x32%.CarStateProtocol.CarStatus.Telemetry\x12\x30\n\x05state\x18\x02 \x01(\x0e\x32!.CarStateProtocol.CarStatus.State\x12$\n\x04stop\x18\x03 \x01(\x0b\x32\x16.CarStateProtocol.Stop\x1a`\n\tTelemetry\x12\r\n\x05speed\x18\x01 \x01(\x01\x12\x0c\n\x04\x66uel\x18\x02 \x01(\x01\x12\x36\n\x08position\x18\x03 \x01(\x0b\x32$.CarStateProtocol.CarStatus.Position\x1a\x41\n\x08Position\x12\x10\n\x08latitude\x18\x01 \x01(\x01\x12\x11\n\tlongitude\x18\x02 \x01(\x01\x12\x10\n\x08\x61ltitude\x18\x03 \x01(\x01\"B\n\x05State\x12\x08\n\x04IDLE\x10\x00\x12\t\n\x05\x44RIVE\x10\x01\x12\x0b\n\x07IN_STOP\x10\x02\x12\x0c\n\x08OBSTACLE\x10\x03\x12\t\n\x05\x45RROR\x10\x04\"\xd7\x01\n\nCarCommand\x12%\n\x05stops\x18\x01 \x03(\x0b\x32\x16.CarStateProtocol.Stop\x12\x33\n\x06\x61\x63tion\x18\x02 \x01(\x0e\x32#.CarStateProtocol.CarCommand.Action\x12\r\n\x05route\x18\x03 \x01(\t\x12\x30\n\rrouteStations\x18\x04 \x03(\x0b\x32\x19.CarStateProtocol.Station\",\n\x06\x41\x63tion\x12\r\n\tNO_ACTION\x10\x00\x12\x08\n\x04STOP\x10\x01\x12\t\n\x05START\x10\x02\"\x12\n\x04Stop\x12\n\n\x02to\x18\x01 \x01(\t\"\x90\x01\n\x07Station\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x34\n\x08position\x18\x02 \x01(\x0b\x32\".CarStateProtocol.Station.Position\x1a\x41\n\x08Position\x12\x10\n\x08latitude\x18\x01 \x01(\x01\x12\x11\n\tlongitude\x18\x02 \x01(\x01\x12\x10\n\x08\x61ltitude\x18\x03 \x01(\x01\x42>Z!../internal/pkg/ba_proto;ba_proto\xaa\x02\x18Google.Protobuf.ba_protob\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'CarStateProtocol_pb2', globals())
@@ -30,9 +30,13 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _CARSTATUS_STATE._serialized_start=369
   _CARSTATUS_STATE._serialized_end=435
   _CARCOMMAND._serialized_start=438
-  _CARCOMMAND._serialized_end=588
-  _CARCOMMAND_ACTION._serialized_start=544
-  _CARCOMMAND_ACTION._serialized_end=588
-  _STOP._serialized_start=590
-  _STOP._serialized_end=608
+  _CARCOMMAND._serialized_end=653
+  _CARCOMMAND_ACTION._serialized_start=609
+  _CARCOMMAND_ACTION._serialized_end=653
+  _STOP._serialized_start=655
+  _STOP._serialized_end=673
+  _STATION._serialized_start=676
+  _STATION._serialized_end=820
+  _STATION_POSITION._serialized_start=302
+  _STATION_POSITION._serialized_end=367
 # @@protoc_insertion_point(module_scope)
--- scripts/third_party_monitoring.py
+++ scripts/third_party_monitoring.py
@@ -37,15 +37,20 @@ def print_status_message_state(state) -> None:
     print("\tstate: %s" % state_string)
 
 
-def print_status_message_position(position) -> None:
-    print("\tposition:")
-    print("\t\tlatitude:  %s" % position.latitude)
-    print("\t\tlongitude: %s" % position.longitude)
-    print("\t\taltitude:  %s" % position.altitude)
+def print_position(position, number_of_tabs: int) -> None:
+    print("\t" * number_of_tabs, end='')
+    print("position:")
+    print(f"\t" * (number_of_tabs + 1), end='')
+    print(f"latitude:  {position.latitude}")
+    print(f"\t" * (number_of_tabs + 1), end='')
+    print(f"longitude: {position.longitude}")
+    print(f"\t" * (number_of_tabs + 1), end='')
+    print(f"altitude:  {position.altitude}")
 
 
-def print_status_message_stop(stop) -> None:
-    print("\tstop: %s" % str(stop.to))
+def print_stop(stop, number_of_tabs: int) -> None:
+    print("\t" * number_of_tabs, end='')
+    print(f"stop: {stop.to}")
 
 
 def print_status_message_server(server) -> None:
@@ -59,21 +64,35 @@ def print_status_message(status_message) -> None:
     print_status_message_server(status_message.server)
     print("Status:")
     print_status_message_state(status_message.carStatus.state)
-    print_status_message_stop(status_message.carStatus.stop)
-    print_status_message_position(status_message.carStatus.telemetry.position)
+    print_stop(status_message.carStatus.stop, 1)
+    print_position(status_message.carStatus.telemetry.position, 1)
 
 
-def print_statusResponse_message(status_response) -> None:
+def print_status_response_message(status_response) -> None:
     print(f"StatusResponse: {status_response.type}\n")
+
+
+def print_command_message(command_message) -> None:
+    print("Command")
+    print("\tstops:")
+    for stop in command_message.carCommand.stops:
+        print_stop(stop, 2)
+    print(f"\taction: {command_message.carCommand.action}")
+    print(f"\troute: {command_message.carCommand.route}")
+    if command_message.carCommand.routeStations:
+        print("\tstations:")
+        for station in command_message.carCommand.routeStations:
+            print(f"\t\tstation: {station.name}")
+            print_position(station.position, 3)
 
 
 def print_message_deamon(message) -> None:
     message_daemon = protocol.MessageDaemon()
     message_daemon.ParseFromString(message.payload)
     if(message_daemon.HasField("commandResponse")):
-        print("commandResponse")
+        print("Command response")
     elif(message_daemon.HasField("connect")):
-        print("connect")
+        print("Connect")
     elif(message_daemon.HasField("status")):
         print_status_message(message_daemon.status)
 
@@ -81,19 +100,20 @@ def print_message_deamon(message) -> None:
 def print_message_industrial_portal(message) -> None:
     message_industrial_portal = protocol.MessageIndustrialPortal()
     message_industrial_portal.ParseFromString(message.payload)
-    if(message_industrial_portal.HasField("connectReponse")):
-        print("connectReponse")
+    if(message_industrial_portal.HasField("connectResponse")):
+        print("Connect response")
     elif(message_industrial_portal.HasField("command")):
-        print("command")
+        print_command_message(message_industrial_portal.command)
     elif(message_industrial_portal.HasField("statusResponse")):
-        print_statusResponse_message(message_industrial_portal.statusResponse)
+        print_status_response_message(message_industrial_portal.statusResponse)
 
 
 def on_message(client, userdata, message) -> None:
     print(f'topic: {message.topic}')
-    if (message.topic.split('/')[-1] == 'daemon'):
+    message_type = message.topic.split('/')[-1]
+    if (message_type == 'daemon'):
         print_message_deamon(message)
-    elif (message.topic.split('/')[-1] == 'industrial_portal'):
+    elif (message_type == 'industrial_portal'):
         print_message_industrial_portal(message)
     else:
         print(f'ERROR:\n\ttopic: {message.topic}\n')
@@ -109,10 +129,10 @@ if __name__ == '__main__':
                          protocol=mqtt.MQTTv5,
                          transport='tcp')
     client.on_message = on_message
-    client.tls_set(ca_certs=args.ca_certs,
-                   certfile=args.certfile,
-                   keyfile=args.keyfile,
-                   tls_version=ssl.PROTOCOL_TLSv1_2)
-    client.connect(args.ip_address, port=args.port, keepalive=60)
+    #client.tls_set(ca_certs=args.ca_certs,
+    #               certfile=args.certfile,
+    #              keyfile=args.keyfile,
+    #               tls_version=ssl.PROTOCOL_TLSv1_2)
+    client.connect("35.206.129.71", 1883, keepalive=60)
     client.subscribe("#", qos=2)
     client.loop_forever(timeout=60)
