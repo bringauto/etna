b1866a19e3ea57b7c4d189c00303933c5a0436f4
mario <mario.ivancik@bringauto.com>
2024-05-10 12:47:08 +0200

    generate volume paths as absolute

--- scripts/create_docker_compose_for_testing.py
+++ scripts/create_docker_compose_for_testing.py
@@ -6,25 +6,34 @@ import os.path
 import subprocess
 
 def main():
-    with open('../docker-compose.yml', 'r') as file:
-        docker_compose = yaml.load(file, Loader=yaml.FullLoader)
     with open('docker_compose_for_testing.json', 'r') as file:
         config = json.load(file)
 
+    etna_path = os.path.abspath(config['etna_path'])
+    with open(os.path.join(etna_path, 'docker-compose.yml'), 'r') as file:
+        docker_compose = yaml.load(file, Loader=yaml.FullLoader)
+
     for component in config['components']:
         if component['replace']:
-            if os.path.isfile(component['path']) and component['path'].endswith('/Dockerfile'):
+            if os.path.isfile(os.path.join(component['path'], 'Dockerfile')):
                 print(f"Building docker image for {component['name']}")
-                command = f"docker build -t {component['name']}:testing -f {component['path']} {component['path'].replace('/Dockerfile', '')}"
+                command = f"docker build -t {component['name']}:testing -f {component['path']}/Dockerfile {component['path']}"
                 rc = subprocess.call(command.split(" "), text=True)
                 if rc != 0:
-                    print(f"Failed to build docker image for {component['name']}")
-                    return
+                   print(f"Failed to build docker image for {component['name']}")
+                   return
                 docker_compose['services'][component['name']]['image'] = f"{component['name']}:testing"
             else:
                 print(f"Path provided for {component['name']} does not lead to a Dockerfile")
                 return
-            
+        volumes = docker_compose['services'][component['name']]['volumes']
+        docker_compose['services'][component['name']]['volumes'] = []
+        for volume in volumes:
+            paths = volume.split(':')
+            docker_compose['services'][component['name']]['volumes'].append(
+                os.path.join(etna_path, paths[0]) + ":" + paths[1]
+            )
+
     with open('docker-compose.yml', 'w') as file:
         file.write(yaml.dump(docker_compose))
 
